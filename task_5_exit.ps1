e8a5a88aca7515b32da5abf3389c5f55a7810234
Eugene Papkou <30703998+EugenePapkou@users.noreply.github.com>
2018-04-20 21:08:01 +0000

    Add files via upload

---
+++ task_5_exit.ps1
@@ -0,0 +1,9 @@
+﻿Get-childitem C:\WebServers -Recurse -File | ForEach-Object {
+     move-Item -Path ($_.FullName) -Destination C:\task5
+
+    [string]$new_name = $_.directory.Name + "_" + (Get-Date -Format o) + 
+    "_" + $_.name -replace (":", "_") -replace ("/", "_") -replace (" ", "_")
+    
+    Rename-Item -path ("C:\task5\" + $_.Name) -NewName $new_name }  
+
+    Get-ChildItem C:\task5 -Recurse | Measure-Object -Sum -Property length
